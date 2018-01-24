37184373a3e2f8b975687858120c6be67555d828
wisaTong <wisa.powthongchin@gmail.com>
2018-01-24 19:37:53 +0700

    Added equals methods and javadoc

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -1,28 +1,60 @@
 package coinpurse;
 
+/**
+ * Coins that contain value and currency.
+ * @author Wisa Powthongchin
+ *
+ */
 public class Coin implements Comparable<Coin> {
 
 	private double value;
 	private String currency;
 	
+	/**
+	 * Create coin object that has value and currency.
+	 * @param value is value of a coin obviously.
+	 * @param currency is String value specify currency type.
+	 */
 	public Coin(double value, String currency) {
 		if (value < 0) throw new IllegalArgumentException("Value cannot be negative");
 		this.value = value;
 		this.currency = currency;
 	}
 	
+	/** 
+	 * Get value of a coin.
+	 * @return value of a coin in double.
+	 */
 	public double getValue() {
 		return value;
 	}
 	
+	/**
+	 * Get currency type of coin.
+	 * @return String value of currency type.
+	 */
 	public String getCurrency() {
 		return currency;
 	}
 	
+	/**
+	 * Compare this coin to other coin object.
+	 */
 	public int compareTo(Coin coin) {
 		if (this.getValue() < coin.getValue()) return -1;
 		else if (this.getValue() > coin.getValue()) return 1;
 		return 0;
+	}
+	
+	/** Ordinary equals method */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (obj.getClass() != this.getClass()) return false;
+		
+		Coin other = (Coin) obj;
+		
+		return this.getCurrency() == other.getCurrency() && this.getValue() == other.getValue();
 	}
 	
 	@Override
