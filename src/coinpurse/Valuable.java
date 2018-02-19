2c469ad89f402da9df449d199adf10ee212f5901
wisaTong <wisa.powthongchin@gmail.com>
2018-02-19 13:49:05 +0700

    Added javadoc

--- src/coinpurse/Valuable.java
+++ src/coinpurse/Valuable.java
@@ -1,8 +1,15 @@
 package coinpurse;
 
+/**
+ * Valuable interface for basic method of future object.
+ * @author Wisa Powthongchin
+ *
+ */
 public interface Valuable extends Comparable<Valuable> {
 
+	/** get double value of monetary object*/
 	public double getValue();
 	
+	/** get String value for currency */
 	public String getCurrency();
 }
--- src/coinpurse/ValueComparator.java
+++ src/coinpurse/ValueComparator.java
@@ -2,8 +2,16 @@ package coinpurse;
 
 import java.util.Comparator;
 
+/**
+ * Class responsible for comparator logic to sort by value and currency
+ * @author Wisa Powthongchin
+ *
+ */
 public class ValueComparator implements Comparator<Valuable> {
 
+	/**
+	 * Compare two Valuable object considering currency and value
+	 */
 	@Override
 	public int compare(Valuable a, Valuable b) {
 		if(a.getCurrency().equals(b.getCurrency())) {
